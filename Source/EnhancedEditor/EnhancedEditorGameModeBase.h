290dc502a004c9b0a920bb7dd8a1264ba80587e3
wguanr <wkuan666@outlook.com>
2023-05-20 21:53:53 +0800

    Initial commit

---
+++ Source/EnhancedEditor/EnhancedEditorGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EnhancedEditorGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ENHANCEDEDITOR_API AEnhancedEditorGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
