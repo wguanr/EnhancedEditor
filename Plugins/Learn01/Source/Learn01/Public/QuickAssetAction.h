961476966dc4e02dffc4e078c540d3c79896969a
wguanr <wkuan666@outlook.com>
2023-05-22 20:23:13 +0800

    chap 2 complete , especially for fix redirectors
    Former-commit-id: 23fcfb3f245c562f36bd6386709d43261368ab96

--- Plugins/Learn01/Source/Learn01/Public/QuickAssetAction.h
+++ Plugins/Learn01/Source/Learn01/Public/QuickAssetAction.h
@@ -3,8 +3,21 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Materials/Material.h"
+#include "Materials/MaterialInstanceConstant.h"
+#include "Particles/ParticleSystem.h"	
+#include "Sound/SoundCue.h"
+#include "Sound/SoundWave.h"
+#include "Engine/Texture.h"
+#include "Blueprint/UserWidget.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "NiagaraSystem.h"
+#include "NiagaraEmitter.h"
+
+
 #include "AssetActionUtility.h"
 #include "QuickAssetAction.generated.h"
+
 
 /**
  * 
@@ -16,4 +29,30 @@ class LEARN01_API UQuickAssetAction : public UAssetActionUtility
 public:
 	UFUNCTION(CallInEditor)
 	void DuplicateAssets(int32 NumOfDuplicates);
+	UFUNCTION(CallInEditor)
+	void AddPrefix();
+
+	UFUNCTION(CallInEditor)
+	void RemoveUnusedAssets();
+private:
+	TMap<UClass*,FString>PrefixMap =
+	{
+		
+		{UBlueprint::StaticClass(),TEXT("BP_")},
+		{UStaticMesh::StaticClass(),TEXT("SM_")},
+		{UMaterial::StaticClass(), TEXT("M_")},
+		{UMaterialInstanceConstant::StaticClass(),TEXT("MI_")},
+		{UMaterialFunctionInterface::StaticClass(), TEXT("MF_")},
+		{UParticleSystem::StaticClass(), TEXT("PS_")},
+		{USoundCue::StaticClass(), TEXT("SC_")},
+		{USoundWave::StaticClass(), TEXT("SW_")},
+		{UTexture::StaticClass(), TEXT("T_")},
+		{UTexture2D::StaticClass(), TEXT("T_")},
+		{UUserWidget::StaticClass(), TEXT("WBP_")},
+		{USkeletalMeshComponent::StaticClass(), TEXT("SK_")},
+		{UNiagaraSystem::StaticClass(), TEXT("NS_")},
+		{UNiagaraEmitter::StaticClass(), TEXT("NE_")}
+	};
+
+	void FixupRedirectors();
 };
