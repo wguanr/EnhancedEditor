9e278a4db9ce2cf7983e374d9b64aa1643d82acc
wguanr <wkuan666@outlook.com>
2023-06-26 14:48:32 +0800

    fix:修改名称
    Former-commit-id: 14f9efd305cd7a373180f7ecc2b2395cb58f14f4

--- EnhancedEditorModV1/Source/EnhancedEditorModV1/Private/QuickAssetAction.cpp
+++ EnhancedEditorModV1/Source/EnhancedEditorModV1/Private/QuickAssetAction.cpp
@@ -12,7 +12,7 @@
 
 void UQuickAssetAction::DuplicateAssets(int32 NumOfDuplicates)
 {
-	if(NumOfDuplicates<=0)
+	if (NumOfDuplicates <= 0)
 	{
 		// Print(TEXT("Please enter a positive number of duplicates"), FColor::Red);
 		DebugHeader::ShowMessageDialog(EAppMsgType::Ok, TEXT("Please enter a positive number of duplicates"));
@@ -22,41 +22,45 @@ void UQuickAssetAction::DuplicateAssets(int32 NumOfDuplicates)
 	TArray<FAssetData> SelectedAssetData = UEditorUtilityLibrary::GetSelectedAssetData();
 	UINT32 Counter = 0;
 
-	for (const FAssetData& SelectAssetData:SelectedAssetData)
+	for (const FAssetData& SelectAssetData : SelectedAssetData)
 	{
-		for(int32 i =0; i<NumOfDuplicates; i++)
+		for (int32 i = 0; i < NumOfDuplicates; i++)
 		{
 			const FString SourceAssetPath = SelectAssetData.GetSoftObjectPath().ToString();
-			const FString NewDuplicatedName = SelectAssetData.AssetName.ToString() + TEXT("_") + FString::FromInt(i+1);
+			const FString NewDuplicatedName = SelectAssetData.AssetName.ToString() + TEXT("_") +
+				FString::FromInt(i + 1);
 			const FString NewPathName = FPaths::Combine(SelectAssetData.PackagePath.ToString(), NewDuplicatedName);
 
-			if(UEditorAssetLibrary::DuplicateAsset(SourceAssetPath, NewPathName))
+			if (UEditorAssetLibrary::DuplicateAsset(SourceAssetPath, NewPathName))
 			{
 				UEditorAssetLibrary::SaveAsset(NewPathName, false);
 				++Counter;
 			}
 		}
 	}
-	
-	if (Counter>0)
+
+	if (Counter > 0)
 	{
-		DebugHeader::ShowNotifyInfo(TEXT("Success duplicated" + FString::FromInt(Counter) + " assets" ) );
+		DebugHeader::ShowNotifyInfo(TEXT("Success duplicated" + FString::FromInt(Counter) + " assets"));
 	}
-	
 }
 
 void UQuickAssetAction::AddPrefix()
 {
 	TArray<UObject*> SelectedAssets = UEditorUtilityLibrary::GetSelectedAssets();
 	UINT32 Counter = 0;
-	for (UObject* SelectedAsset:SelectedAssets)
+	for (UObject* SelectedAsset : SelectedAssets)
 	{
-		if (!SelectedAsset) continue;
+		if (!SelectedAsset)
+		{
+			continue;
+		}
 		// this will return a ** pointer ** to the key value
 		FString* PrefixFound = PrefixMap.Find(SelectedAsset->GetClass());
 		if (!PrefixFound || PrefixFound->IsEmpty())
 		{
-			DebugHeader::Print(TEXT("Prefix doesnt exist for asset type :" + SelectedAsset->GetClass()->GetName()),FColor::Red);
+			DebugHeader::Print(
+				TEXT("Prefix doesnt exist for asset type :" + SelectedAsset->GetClass()->GetName()), FColor::Red);
 			continue;
 		}
 
@@ -64,7 +68,7 @@ void UQuickAssetAction::AddPrefix()
 		// check the string using most FString functions
 		if (OldAssetName.StartsWith(*PrefixFound))
 		{
-			DebugHeader::Print(TEXT("Prefix already exists for asset :"+ SelectedAsset->GetName()),FColor::Red);
+			DebugHeader::Print(TEXT("Prefix already exists for asset :"+ SelectedAsset->GetName()), FColor::Red);
 			continue;
 		}
 		if (SelectedAsset->IsA<UMaterialInstanceConstant>())
@@ -72,18 +76,16 @@ void UQuickAssetAction::AddPrefix()
 			OldAssetName.RemoveFromStart(TEXT("M_"));
 			OldAssetName.RemoveFromEnd(TEXT("_inst"));
 		}
-		
+
 
 		const FString NewAssetPath = *PrefixFound + OldAssetName;
 
 		UEditorUtilityLibrary::RenameAsset(SelectedAsset, NewAssetPath);
 
 		++Counter;
-		
 	}
-	
+
 	DebugHeader::ShowNotifyInfo(TEXT("I have renamed " + FString::FromInt(Counter) + " assets"));
-	
 }
 
 void UQuickAssetAction::RemoveUnusedAssets()
@@ -92,13 +94,14 @@ void UQuickAssetAction::RemoveUnusedAssets()
 	TArray<FAssetData> UnusedAssetData;
 
 	FixupRedirectors();
-	
-	for(const FAssetData& AssetData:SelectedAssetData)
+
+	for (const FAssetData& AssetData : SelectedAssetData)
 	{
 		// remember to saved level and others first
-		TArray<FString> ReferencesForAsset = UEditorAssetLibrary::FindPackageReferencersForAsset(AssetData.GetObjectPathString());
+		TArray<FString> ReferencesForAsset = UEditorAssetLibrary::FindPackageReferencersForAsset(
+			AssetData.GetObjectPathString());
 
-		if(ReferencesForAsset.Num()==0)
+		if (ReferencesForAsset.Num() == 0)
 		{
 			UnusedAssetData.Add(AssetData);
 		}
@@ -106,23 +109,26 @@ void UQuickAssetAction::RemoveUnusedAssets()
 
 	if (UnusedAssetData.Num() == 0)
 	{
-		DebugHeader::ShowMessageDialog(EAppMsgType::Ok,TEXT("Find nothing Unused"),false);
+		DebugHeader::ShowMessageDialog(EAppMsgType::Ok,TEXT("Find nothing Unused"), false);
 		return;
 	}
 
 	// main
-	const int32 NumOfAssetsDeleted = ObjectTools::DeleteAssets(UnusedAssetData,false);
-	if(NumOfAssetsDeleted ==0) return;
+	const int32 NumOfAssetsDeleted = ObjectTools::DeleteAssets(UnusedAssetData, false);
+	if (NumOfAssetsDeleted == 0)
+	{
+		return;
+	}
 
 	DebugHeader::ShowNotifyInfo(TEXT("Successfully deleted " + FString::FromInt(NumOfAssetsDeleted) + "Assets"));
-	
 }
 
 void UQuickAssetAction::FixupRedirectors()
 {
 	TArray<UObjectRedirector*> RedirectorsToFix;
-	
-	FAssetRegistryModule& AssetRegistryModule = FModuleManager::Get().LoadModuleChecked<FAssetRegistryModule>(TEXT("AssetRegistry"));
+
+	FAssetRegistryModule& AssetRegistryModule = FModuleManager::Get().LoadModuleChecked<FAssetRegistryModule>(
+		TEXT("AssetRegistry"));
 
 	FARFilter Filter;
 	Filter.bRecursivePaths = true;
@@ -131,18 +137,18 @@ void UQuickAssetAction::FixupRedirectors()
 
 	TArray<FAssetData> OutAssetData;
 	// the module reference the OutAssetData var to get the right assets
-	AssetRegistryModule.Get().GetAssets(Filter,OutAssetData);
+	AssetRegistryModule.Get().GetAssets(Filter, OutAssetData);
 
-	for ( const FAssetData AssetData:OutAssetData)
+	for (const FAssetData AssetData : OutAssetData)
 	{
-		if(UObjectRedirector* RedirectorToFix = Cast<UObjectRedirector>(AssetData.GetAsset()))
+		if (UObjectRedirector* RedirectorToFix = Cast<UObjectRedirector>(AssetData.GetAsset()))
 		{
 			RedirectorsToFix.Add(RedirectorToFix);
 		}
-		
 	}
 
-	
-	FAssetToolsModule& AssetToolsModule = FModuleManager::Get().LoadModuleChecked<FAssetToolsModule>(TEXT("AssetTools"));
+
+	FAssetToolsModule& AssetToolsModule = FModuleManager::Get().LoadModuleChecked<
+		FAssetToolsModule>(TEXT("AssetTools"));
 	AssetToolsModule.Get().FixupReferencers(RedirectorsToFix);
 }
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # EnhancedEditor
- MyLearning of scripts
+自学的拓展编辑器功能插件，UE 5.1 第一版。
+
+
