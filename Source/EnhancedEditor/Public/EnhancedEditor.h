ce8f26cf798ececa642b0505b0fb2cacc9eb5c1c
Guanr <wkuan666@outlook.com>
2024-09-19 17:49:08 +0800

    整合模块

--- Source/EnhancedEditor/Public/EnhancedEditor.h
+++ Source/EnhancedEditor/Public/EnhancedEditor.h
@@ -1,4 +1,6 @@
-﻿#pragma once
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
 
 #include "CoreMinimal.h"
 #include "Modules/ModuleManager.h"
@@ -6,6 +8,42 @@
 class FEnhancedEditorModule : public IModuleInterface
 {
 public:
-    virtual void StartupModule() override;
-    virtual void ShutdownModule() override;
+	/** IModuleInterface implementation */
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+
+private:
+#pragma region ContentBrowserMenuEXtension
+
+	void IniCBMenuExtension();
+
+	TArray<FString> FolderPathSelected;
+
+	TSharedRef<FExtender> CustomCBMenuExtender(const TArray<FString>& SelectedPaths);
+	void AddCustomMenuEntry(class FMenuBuilder& MenuBuilder);
+	void OnCleanUselessShit();
+	void OnDeleteUnusedAssetsButtonCliked();
+	void OnDeleteUnusedFoldersButtonCliked();
+
+	//Slat
+	void OnOpenSlatePannel();
+
+	void FixupRedirectors();
+#pragma endregion
+
+#pragma region CustomEditorTab
+
+	void RegisterAdvanceDeletionTab();
+
+	TSharedRef<SDockTab> OnSpawnAdvancedDeletionTab(const FSpawnTabArgs& SpawnTabArgs);
+
+	TArray<TSharedPtr<FAssetData>> GetAssetData();
+#pragma endregion
+
+public:
+#pragma region ProcessDataForAdvanceDeletionTab
+
+	bool DeleteSingleAssetBySlate(const FAssetData& AssetDataToDelete);
+	bool DeleteMultipleAssetsBySlate(const TArray<FAssetData>& AssetsDataToDelete);
+#pragma endregion
 };
